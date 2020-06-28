c5fe6754714a2678d77151b98caeb2f5ca735f46
BrunoConti <bruno.conti1997@gmail.com>
2020-06-28 19:29:25 -0300

    Add: agrego ruteo de propiedad

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from './signIn'
+import Property from './property';
 
 const Routes = props => {
 	return (
@@ -8,6 +9,7 @@ const Routes = props => {
 			<Switch>
 				<Route exact path="/">Home</Route>
 				<Route path="/sign-in" exact component={SignIn} />
+				<Route path="/property" exact component={Property} />
 			</Switch>
 		</Router>
 	);
