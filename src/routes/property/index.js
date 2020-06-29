76cadf7838297cae21e0cfcd3e77697db6973ab2
BrunoConti <bruno.conti1997@gmail.com>
2020-06-28 23:15:19 -0300

    Add: Agrego estilo y campos a property

--- src/routes/property/index.js
+++ src/routes/property/index.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { Form, Input, Button, notification } from "antd";
+import { Form, Input, Button, notification, Checkbox, Upload, Select } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 const FormProperty = () => {
-    const [ form ] = Form.useForm()
+  const contractOptions = [ "Dueño", "Inmobiliaria" ];
+
+  const { Option } = Select;
+
+  const [form] = Form.useForm();
+
   const onFinish = (values) => {
     console.log("Success:", values);
     notification.success({
-        message: 'Se cargo con éxito',
-        description:
-          'Se agrego correctamente la propiedad',
-        placement: 'bottomLeft'
-      });
+      message: "Se cargo con éxito",
+      description: "Se agrego correctamente la propiedad",
+      placement: "bottomLeft",
+    });
   };
 
   const onFinishFailed = (errorInfo) => {
     console.log("Failed:", errorInfo);
     notification.error({
-        message: 'No se cargo la propiedad',
-        description:
-          'Ocurrio un error al cargar la propiedad',
-        placement: 'bottomLeft'
-      });
+      message: "No se cargo la propiedad",
+      description: "Ocurrio un error al cargar la propiedad",
+      placement: "bottomLeft",
+    });
   };
   return (
     <Form
@@ -31,7 +35,7 @@ const FormProperty = () => {
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       form={form}
-      layout='vertical'
+      layout="vertical"
     >
       <Form.Item
         label="Titulo"
@@ -45,58 +49,96 @@ const FormProperty = () => {
       >
         <Input />
       </Form.Item>
+      <div className="group">
+        <Form.Item
+          label="Dirección"
+          name="propertyAddress"
+          rules={[
+            {
+              required: true,
+              message: "Please input property address!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Dirección"
-        name="propertyAddress"
-        rules={[
-          {
-            required: true,
-            message: "Please input property address!",
-          },
-        ]}
-      >
-        <Input />
+        <Form.Item
+          label="Habitaciones"
+          name="propertyRooms"
+          rules={[
+            {
+              required: true,
+              message: "Please input property rooms!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </div>
+
+      <Form.Item name="propertyAttributes" label="Comodidades">
+        <Checkbox.Group>
+          <Checkbox value="A">Ascensor</Checkbox>
+          <Checkbox value="B">Patio</Checkbox>
+          <Checkbox value="C">WiFI</Checkbox>
+          <Checkbox value="D">Asador</Checkbox>
+          <Checkbox value="E">Balcon</Checkbox>
+          <Checkbox value="F">Cochera</Checkbox>
+        </Checkbox.Group>
       </Form.Item>
+      <div className="group">
 
       <Form.Item
-        label="Ambientes"
-        name="propertyRooms"
-        rules={[
-          {
-            required: true,
-            message: "Please input property rooms!",
-          },
-        ]}
+        name="propertyContract"
+        label="Tipo de Contrato"
       >
-        <Input />
+        <Select placeholder="Selecciona el tipo de contrato" allowClear>
+            {contractOptions.map((c) => {
+                return(
+                    <Option key={c} value={c}>
+                        {c}
+                    </Option>
+                )
+            })}
+          
+        </Select>
+
       </Form.Item>
+        <Form.Item
+          label="Precio"
+          name="propertyPrice"
+          rules={[
+            {
+              required: true,
+              message: "Please input property price!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </div>
 
-      <Form.Item
-        label="Precio"
-        name="propertyPrice"
-        rules={[
-          {
-            required: true,
-            message: "Please input property price!",
-          },
-        ]}
-      >
-        <Input />
+      <Form.Item name="images" label="Imagenes">
+        <Upload name="logo" listType="picture">
+          <Button>
+            <UploadOutlined /> Elegir imagenes
+          </Button>
+        </Upload>
       </Form.Item>
 
       <Form.Item>
-				<Button type="primary" htmlType="submit">
-					Publicar
-				</Button>
-			</Form.Item>
+        <Button type="primary" htmlType="submit">
+          Publicar
+        </Button>
+      </Form.Item>
     </Form>
   );
 };
 
 const Property = (props) => {
   return (
-    <div className='form-property'>
+    <div className="form-property">
       <FormProperty />
     </div>
   );
