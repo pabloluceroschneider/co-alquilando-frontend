790df7392fbb48c5687ccc3557b0aa42da0e53e0
BrunoConti <bruno.conti1997@gmail.com>
2020-06-30 16:49:36 -0300

    Add: routeo al perfil de usuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from './signIn'
+import Profile from './profile';
 
 const Routes = props => {
 	return (
@@ -8,6 +9,7 @@ const Routes = props => {
 			<Switch>
 				<Route exact path="/">Home</Route>
 				<Route path="/sign-in" exact component={SignIn} />
+				<Route path="/profile/:nickname" exact component={Profile} />
 			</Switch>
 		</Router>
 	);
