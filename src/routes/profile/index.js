ee6ec5248a628c0f580882382ff9d75c9f571268
BrunoConti <bruno.conti1997@gmail.com>
2020-07-02 21:45:04 -0300

    Add: primer version de visualizar perfil

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ApiRequest } from "../../util/ApiRequest";
+import { Button } from "antd";
 
-const Profile = props => {
-    let {nickname} = useParams();
-    console.log(nickname);
-    return (
-        <div>
-            Profile: {nickname}
+const image =
+  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png";
+
+const Profile = (props) => {
+  let { nickname } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const userData = ApiRequest.get(`/users/${nickname}`);
+    setData(userData.data);
+    console.log(data);
+  }, [nickname, data]);
+
+  return (
+    <>
+      <div className="profileContent">
+        <div className="group2">
+          <div className="imageContent">
+            <img alt="imagen de perfil" src={image} className="profileImage" />
+            <h2>
+              <strong>Michael Scofield</strong>
+            </h2>
+            <h4>Córdoba - Argentina</h4>
+          </div>
+          <div className="datosContent">
+            <p>Nickname: {nickname}</p>
+            <p>Edad: 28</p>
+            <p>Descripcion: Aqui va la Descripcion</p>
+            <Button type="primary">Conectar</Button>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Profile;
---
+++ src/util/Api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const url = {
+    local: "http://localhost:8080/"
+}
+
+export default axios.create({
+    baseURL: url.local,
+    headers:{
+        'Content-Type':'application/json'
+    }
+})
