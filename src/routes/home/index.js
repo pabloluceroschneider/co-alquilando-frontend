b403ae211250a16b8f6c34ab3c1a055d9178d4a6
Santiago Costa <santiago.costa.m@gmail.com>
2020-07-04 16:59:34 -0300

    Fix

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import Carrousel from '../components/reusable/carrousel/index'
-import image1 from '../components/assets/images/Home/1.jpg'
-import image2 from '../components/assets/images/Home/2.jpg'
-import image3 from '../components/assets/images/Home/3.jpg'
-import image4 from '../components/assets/images/Home/4.jpg'
-import image5 from '../components/assets/images/Home/bloque1_1.jpg'
-import image6 from '../components/assets/images/Home/bloque1_2.jpg'
-import image7 from '../components/assets/images/Home/bloque1_3.jpg'
-import Menu from '../components/menu/index'
-import Body2 from '../components/reusable/body2/index'
-import Body1 from '../components/reusable/body1/index'
+import Carrousel from '../../components/reusable/carrousel/index'
+import image1 from '../../assets/images/Home/1.jpg'
+import image2 from '../../assets/images/Home/2.jpg'
+import image3 from '../../assets/images/Home/3.jpg'
+import image4 from '../../assets/images/Home/4.jpg'
+import image5 from '../../assets/images/Home/bloque1_1.jpg'
+import image6 from '../../assets/images/Home/bloque1_2.jpg'
+import image7 from '../../assets/images/Home/bloque1_3.jpg'
+import Menu from '../../components/menu/index'
+import Body2 from '../../components/reusable/body2/index'
+import Body1 from '../../components/reusable/body1/index'
 import { Divider } from 'antd';
 
 const Home = () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from './signIn'
-<<<<<<< HEAD
-import Home from '../home/index'
+import Home from './home'
 import Menu from '../components/menu/index'
 import Header from '../components/header/index'
 import Footer from '../components/footer/index'
-=======
 import Property from './property';
->>>>>>> eb4eba03f5c7aa5d4814477e910211a4e2bf7df1
 
 const Routes = props => {
 	return (
