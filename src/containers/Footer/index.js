f50b3fe3431b2f30929d28a186de0cc3b7662990
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-07-06 19:13:59 -0300

    style: Mejoro estilos de footer

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 const foot = () => {
   return (
 
-    <div className= "col-sm-12 site-page-foot">
+    <footer className= "col-sm-12 site-page-foot">
      
-    </div>
+    </footer>
   );
 }
 
