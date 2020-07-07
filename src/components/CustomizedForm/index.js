8d6342e721acd7bd65982093d8b09194b111d218
BrunoConti <bruno.conti1997@gmail.com>
2020-07-06 21:30:33 -0300

    Add: customized form a property

--- src/components/CustomizedForm/index.js
+++ src/components/CustomizedForm/index.js
@@ -1,135 +1,168 @@
-import React, { useState } from 'react';
-import { Form, Button, notification, Divider } from 'antd';
-import InputRepository from '../InputRepository';
+import React, { useState } from "react";
+import { Form, Button, notification, Divider } from "antd";
+import InputRepository from "../InputRepository";
 
 const Row = (props) => {
-	const { fields } = props;
-	return (
-		<div className={`group${fields.length}`}>
-			{fields.map((element) => {
-				return (
-					<Form.Item
-						key={element.label}
-						label={element.label}
-						name={element.name}
-						dependencies={element.dependencies}
-						hasFeedback={element.hasFeedback}
-						valuePropName={element.valuePropName}
-						rules={ element.validate ?
-							[
-								{
-									required: element.required,
-									message: `Porfavor, ingrese ${element.label}`
-								},
-								element.validate ? element.validate : () => { return Promise.resolve() }
-						] : [{
-							required: element.required,
-							message: `Porfavor, ingrese ${element.label}`
-						}]
-						}
-					>
-						{InputRepository(element)}
-					</Form.Item>
-				);
-			})}
-		</div>
-	);
+  const { fields } = props;
+  return (
+    <div className={`group${fields.length}`}>
+      {fields.map((element) => {
+        return (
+          <Form.Item
+            key={element.label}
+            label={element.label}
+            name={element.name}
+            dependencies={element.dependencies}
+            hasFeedback={element.hasFeedback}
+            valuePropName={element.valuePropName}
+            rules={
+              element.validate
+                ? [
+                    {
+                      required: element.required,
+                      message: `Porfavor, ingrese ${element.label}`,
+                    },
+                    element.validate
+                      ? element.validate
+                      : () => {
+                          return Promise.resolve();
+                        },
+                  ]
+                : [
+                    {
+                      required: element.required,
+                      message: `Porfavor, ingrese ${element.label}`,
+                    },
+                  ]
+            }
+          >
+            {InputRepository(element)}
+          </Form.Item>
+        );
+      })}
+    </div>
+  );
 };
 
 const CustomizedForm = (props) => {
-	const { data, onfinish, form } = props;
-	const { name, layout, fields, btnSubmit } = data;
-	const { primaries, secondaries, tertiaries } = fields;
-	const [ showSecondary, setShowSecondary ] = useState(false);
-	const [ showTertiary, setShowTertiary ] = useState(false);
+  const { data, onfinish, form } = props;
+  const { name, layout, fields, btnSubmit } = data;
+  const { primaries, secondaries, tertiaries } = fields;
+  const [showSecondary, setShowSecondary] = useState(false);
+  const [showTertiary, setShowTertiary] = useState(false);
 
-	const onFinish = (values) => {
-		onfinish(values)
-	};
+  const onFinish = (values) => {
+    onfinish(values);
+  };
 
-	const onFinishFailed = values => {
-		notification.error({
-			message: `Error en formulario`,
-			placement: 'bottomLeft'
-		});
-	};
+  const onFinishFailed = (values) => {
+    notification.error({
+      message: `Error en formulario`,
+      placement: "bottomLeft",
+    });
+  };
 
-	const showMore = () => {
-		if(secondaries){
-			if(showSecondary){
-				if(tertiaries){
-					if(showTertiary){
-						return <div onClick={ () => {setShowTertiary(false);setShowSecondary(false)}}>Mostrar menos</div>
-					}else{
-						return <div onClick={ () => {setShowTertiary(true)}}>Mostrar más</div>
-					}
-				}else{
-					return <div onClick={ () => {setShowSecondary(false)}}>Mostrar menos</div>
-				}
+  const showMore = () => {
+    if (secondaries) {
+      if (showSecondary) {
+        if (tertiaries) {
+          if (showTertiary) {
+            return (
+              <div
+                onClick={() => {
+                  setShowTertiary(false);
+                  setShowSecondary(false);
+                }}
+              >
+                Mostrar menos
+              </div>
+            );
+          } else {
+            return (
+              <div
+                onClick={() => {
+                  setShowTertiary(true);
+                }}
+              >
+                Mostrar más
+              </div>
+            );
+          }
+        } else {
+          return (
+            <div
+              onClick={() => {
+                setShowSecondary(false);
+              }}
+            >
+              Mostrar menos
+            </div>
+          );
+        }
+      } else {
+        return (
+          <div
+            onClick={() => {
+              setShowSecondary(true);
+            }}
+          >
+            Mostrar más
+          </div>
+        );
+      }
+    } else {
+      return null;
+    }
+  };
 
-			}else{
-				return <div onClick={ () => {setShowSecondary(true)} }>Mostrar más</div>
-			}
-		}else{
-			return null
-		}
-	}
+  const renderForm = () => {
+    return (
+      <Form
+        name={name}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        form={form}
+        layout={layout}
+      >
+        {data
+          ? primaries.map((row, index) => {
+              return <Row key={index} fields={row} />;
+            })
+          : null}
 
-	const renderForm = () => {
-		return (
-			<Form
-				name={name}
-				initialValues={{
-					remember: true
-				}}
-				onFinish={onFinish}
-				onFinishFailed={onFinishFailed}
-				form={form}
-				layout={layout}
-			>
-				{data ? (
-					primaries.map((row, index) => {
-						return <Row key={index} fields={row} />;
-					})
-				) : null}
+        {showSecondary ? (
+          <>
+            <Divider />
+            {secondaries.map((row, index) => {
+              return <Row key={index} fields={row} />;
+            })}
+          </>
+        ) : null}
 
-				{showSecondary ? (
-					<>
-					<Divider />
-					{secondaries.map((row, index) => {
-						return <Row key={index} fields={row} />;
-					})}
-					</>
-				) : null}
+        {showTertiary ? (
+          <>
+            <Divider />
+            {tertiaries.map((row, index) => {
+              return <Row key={index} fields={row} />;
+            })}
+          </>
+        ) : null}
 
-				{showTertiary ? (
-					<>
-					<Divider />
-					{tertiaries.map((row, index) => {
-						return <Row key={index} fields={row}/>;
-					})}
-					</>
-				) : null}
+        <div className="showmore">{showMore()}</div>
 
-				<div className="showmore">
-					{showMore()}
-				</div>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            {btnSubmit}
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  };
 
-				<Form.Item>
-					<Button type="primary" htmlType="submit">
-						{btnSubmit}
-					</Button>
-				</Form.Item>
-			</Form>
-
-		)
-	}
-
-	return (
-		<div className="customizedForm">
-			{renderForm()}
-		</div>
-	);
+  return <div className="customizedForm">{renderForm()}</div>;
 };
 
 export default CustomizedForm;
--- src/routes/property/index.js
+++ src/routes/property/index.js
@@ -1,12 +1,124 @@
 import React from "react";
-import { Form, Input, Button, notification, Checkbox, Upload, Select } from "antd";
-import { UploadOutlined } from "@ant-design/icons";
+import { Form, notification } from "antd";
+// import { UploadOutlined } from "@ant-design/icons";
+import CustomizedForm from "../../components/CustomizedForm";
+import ContentWrapper from "../../components/ContentWrapper";
 
-const FormProperty = () => {
-  const contractOptions = [ "Dueño", "Inmobiliaria" ];
+const propertyData = {
+  name: "property",
+  layout: "vertical",
+  btnSubmit: "Registrar Propiedad",
+  fields: {
+    primaries: [
+      [
+        {
+          label: "Título",
+          name: "propertyTitle",
+          component: "Input",
+          required: true,
+        },
+      ],
+      [
+        {
+          label: "Dirección",
+          name: "propertyAddress",
+          component: "Input",
+          required: true,
+        },
+        {
+          label: "Habitaciones",
+          name: "propertyRooms",
+          component: "Input",
+          required: true,
+        },
+      ],
+      // [
+      // 	{
+      // 		label: "Comodidades",
+      // 		name: "propertyAmenities",
+      // 		component: "Checkbox",
+      //   },
+      //   {
+      // 		label: "",
+      // 		name: "propertyAmenities",
+      // 		component: "Checkbox",
+      //   },
+      //   {
+      // 		name: "propertyAmenities",
+      // 		component: "Checkbox",
+      //   },
+      //   {
+      // 		label: "",
+      // 		name: "propertyAmenities",
+      // 		component: "Checkbox",
+      // 	},
+      // ],
+      [
+        {
+          label: "Tipo de Contrato",
+          name: "propertyContract",
+          component: "Select",
+          options: [
+            { name: "Dueño", value: "DUEÑO" },
+            { name: "Inmobiliaria", value: "INMOBILIARIA" },
+          ],
+          required: true,
+        },
+        {
+          label: "Precio",
+          name: "propertyPrice",
+          component: "Input",
+          required: true,
+        },
+      ],
+    ],
+    secondaries: [
+      [
+        {
+          label: "Documento de Identidad",
+          name: "userDni",
+          component: "Input",
+        },
+        {
+          label: "Sexo",
+          name: "userSex",
+          component: "Select",
+          options: [
+            { name: "Femenino", value: "FEMALE" },
+            { name: "Masculino", value: "MALE" },
+            { name: "Otro", value: "NOT_DEFINED" },
+          ],
+        },
+      ],
+      [
+        {
+          label: "Nacionalidad",
+          name: "userNationality",
+          component: "Input",
+        },
+        {
+          label: "Ciudad",
+          name: "userCity",
+          component: "Input",
+        },
+      ],
+      [
+        {
+          label: "Descripción Personal",
+          name: "userDescription",
+          component: "Input.TextArea",
+        },
+        {
+          label: "Cargar Imagen",
+          name: "userPhoto",
+          component: "Upload",
+        },
+      ],
+    ],
+  },
+};
 
-  const { Option } = Select;
-
+const Property = () => {
   const [form] = Form.useForm();
 
   const onFinish = (values) => {
@@ -18,130 +130,12 @@ const FormProperty = () => {
     });
   };
 
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-    notification.error({
-      message: "No se cargo la propiedad",
-      description: "Ocurrio un error al cargar la propiedad",
-      placement: "bottomLeft",
-    });
-  };
   return (
-    <Form
-      name="propery"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      form={form}
-      layout="vertical"
-    >
-      <Form.Item
-        label="Titulo"
-        name="propertyTitle"
-        rules={[
-          {
-            required: true,
-            message: "Please input property title!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <div className="group">
-        <Form.Item
-          label="Dirección"
-          name="propertyAddress"
-          rules={[
-            {
-              required: true,
-              message: "Please input property address!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Habitaciones"
-          name="propertyRooms"
-          rules={[
-            {
-              required: true,
-              message: "Please input property rooms!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </div>
-
-      <Form.Item name="propertyAttributes" label="Comodidades">
-        <Checkbox.Group>
-          <Checkbox value="A">Ascensor</Checkbox>
-          <Checkbox value="B">Patio</Checkbox>
-          <Checkbox value="C">WiFI</Checkbox>
-          <Checkbox value="D">Asador</Checkbox>
-          <Checkbox value="E">Balcon</Checkbox>
-          <Checkbox value="F">Cochera</Checkbox>
-        </Checkbox.Group>
-      </Form.Item>
-      <div className="group">
-
-      <Form.Item
-        name="propertyContract"
-        label="Tipo de Contrato"
-      >
-        <Select placeholder="Selecciona el tipo de contrato" allowClear>
-            {contractOptions.map((c) => {
-                return(
-                    <Option key={c} value={c}>
-                        {c}
-                    </Option>
-                )
-            })}
-          
-        </Select>
-
-      </Form.Item>
-        <Form.Item
-          label="Precio"
-          name="propertyPrice"
-          rules={[
-            {
-              required: true,
-              message: "Please input property price!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-      </div>
-
-      <Form.Item name="images" label="Imagenes">
-        <Upload name="logo" listType="picture">
-          <Button>
-            <UploadOutlined /> Elegir imagenes
-          </Button>
-        </Upload>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Publicar
-        </Button>
-      </Form.Item>
-    </Form>
-  );
+    <ContentWrapper header footer>
+      <CustomizedForm form={form} data={propertyData} onfinish={onFinish} />
+    </ContentWrapper>
+  )
 };
 
-const Property = (props) => {
-  return (
-    <div className="form-property">
-      <FormProperty />
-    </div>
-  );
-};
 
 export default Property;
