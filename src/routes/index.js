532d3dd5411d2faa3d3a6a4691031fca4dc98100
agustinperalta <43344932+agustinperalta@users.noreply.github.com>
2020-07-17 18:16:50 -0300

    Add: ModificarPreferenciasUserConBug

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import SignIn from "./signIn";
 import Profile from "./profile/index";
 import UpdateForm from "./profile/updateProfile";
 import Property from "./property";
+import UpdatePreferenciesForm from './profile/updatePreferenciesProfile';
 const Routes = () => {
   return (
     <Router>
@@ -12,6 +13,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route path="/sign-in" exact component={SignIn} />
         <Route path="/profile/:nickname/update" component={UpdateForm} />
+        <Route path="/profile/:nickname/updatePreferencies" component={UpdatePreferenciesForm} />
         <Route path="/profile/:nickname" component={Profile} />
         <Route path="/property" exact component={Property} />
       </Switch>
--- src/routes/profile/updatePreferenciesProfile.js
+++ src/routes/profile/updatePreferenciesProfile.js
@@ -1,52 +1,285 @@
-const userPreferencies = {
-    name: "user",
-    layout: "vertical",
-    btnSubmit: "Actualizar Preferencias",
-    fields: {
-      primaries: [
-        [
-          {
-            label: "Preferencias de Roomie",
-            component: "h2",
-          },
-        ],
-        [
-            {
-                label: "Sexo",
-                name: "userSex",
-                component: "Select",
-                options: [
-                  { name: "Femenino", value: "FEMALE" },
-                  { name: "Masculino", value: "MALE" },
-                  { name: "Otro", value: "NOT_DEFINED" },
-                ],
-            },
-            {
-                label:"Edad",
-                name:"userAge",
-                component:"Input"
-            }
-        ],
-        [
-          {
-            label: "Nacionalidad",
-            name: "userNationality",
-            component: "Input",
-          },
-         {
-             label: "Ciudad",
-             name: "userCity",
-             component:"Input"
-         }
-        ],
-        [
-          {
-            label: "Ocupación",
-            name: "userOcupation",
-            component: "Input",
-          },
-         
-        ],
+import React, { useEffect, useState } from "react";
+import ContentWrapper from "../../components/ContentWrapper";
+import CustomizedForm from "../../components/CustomizedForm";
+import { Form, notification } from "antd";
+import { useParams, useHistory } from "react-router-dom";
+import ApiRequest from "../../util/ApiRequest";
+
+const userPreferenciesRoomie = {
+  name: "user",
+  layout: "vertical",
+  btnSubmit: "Actualizar Preferencias de Roomie",
+  className: "userPreferencies",
+  fields: {
+    primaries: [
+      [
+        {
+          label: "Preferencias de Roomie",
+          component: "h2",
+        },
       ],
-    },
-  };
+      [
+        {
+          label: "Sexo",
+          name: ["roommatePreferences", "sex"],
+          component: "Select",
+          options: [
+            { name: "Femenino", value: "FEMALE" },
+            { name: "Masculino", value: "MALE" },
+            { name: "Otro", value: "NOT_DEFINED" },
+          ],
+        },
+        {
+          label: "Edad Minima",
+          name: ["roommatePreferences", "minAge"],
+          component: "Input",
+        },
+        {
+          label: "Edad Máxima",
+          name: ["roommatePreferences", "maxAge"],
+          component: "Input",
+        },
+      ],
+      [
+        {
+          label: "Nacionalidad",
+          name: ["roommatePreferences", "nationality"],
+          component: "Input",
+        },
+        {
+          label: "Ciudad",
+          name: ["roommatePreferences", "city"],
+          component: "Input",
+        },
+      ],
+      [
+        {
+          label: "Ocupación",
+          name: ["roommatePreferences", "occupation"],
+          component: "Input",
+        },
+        {
+          label: "Mascotas",
+          name: ["roommatePreferences", "pets"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+      ],
+    ],
+  },
+};
+
+const userPreferenciesProperty = {
+  name: "user",
+  layout: "vertical",
+  btnSubmit: "Actualizar Preferencias de Propiedad",
+  className: "userPreferencies",
+  fields: {
+    primaries: [
+      [
+        {
+          label: "Preferencias de Propiedad",
+          component: "h2",
+        },
+      ],
+      [
+        {
+          label: "Barrio",
+          name: ["propertyPreferences", "address"],
+          component: "Input",
+        },
+        {
+          label: "Tipología",
+          name: ["propertyPreferences", "typology"],
+          component: "Select",
+          options: [
+            { name: "Departamento", value: "APARMENT" },
+            { name: "Casa", value: "HOUSE" },
+            { name: "Otro", value: "NOT_DEFINED" },
+          ],
+        },
+      ],
+      [
+        {
+          label: "Habitaciones",
+          name: ["propertyPreferences", "rooms"],
+          component: "Input",
+        },
+        {
+          label: "Baños",
+          name: ["propertyPreferences", "bathrooms"],
+          component: "Input",
+        },
+      ],
+      [
+        {
+          label: "Gimnasio",
+          name: ["propertyPreferences", "gym"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Pileta",
+          name: ["propertyPreferences", "pool"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Playroom",
+          name: ["propertyPreferences", "playroom"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+      ],
+      [
+        {
+          label: "Asador",
+          name: ["propertyPreferences", "roaster"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Cochera",
+          name: ["propertyPreferences", "garage"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Balcón",
+          name: ["propertyPreferences", "balcony"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+      ],
+      [
+        {
+          label: "Ascensor",
+          name: ["propertyPreferences", "elevator"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Amoblado Incluido",
+          name: ["propertyPreferences", "furnished"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Aire acondicionado",
+          name: ["propertyPreferences", "aa"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+      ],
+      [
+        {
+          label: "Salon de usos múltiples",
+          name: ["propertyPreferences", "sum"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+        {
+          label: "Calefacción",
+          name: ["propertyPreferences", "calefaction"],
+          valuePropName: "checked",
+          component: "Checkbox",
+        },
+      ],
+    ],
+  },
+};
+
+const UpdatePreferenciesForm = (props) => {
+  const [formRoom] = Form.useForm();
+  const [formProp] = Form.useForm();
+  const [fieldsRoom, setFieldsRoom] = useState(null);
+  const [fieldsProp, setFieldsProp] = useState(null);
+  let { nickname } = useParams();
+  const [idUser,setIdUser] = useState(null);
+  useEffect(() => {
+    let asyncGetUser = async () => {
+      await ApiRequest.get(`/user/${nickname}`).then((res) => {
+        let { data } = res;
+        setIdUser(data.id);
+        data = data.preferences;
+        let arrayRoommate = [];
+        let arrayProperty = [];
+        data.roommatePreferences.attributes.forEach((t) => {
+          arrayRoommate.push({ [t.attributeType]: t.value });
+        });
+        data.propertyPreferences.attributes.forEach((t) => {
+          arrayProperty.push({ [t.attributeType]: t.value });
+        });
+
+        delete data.propertyPreferences;
+        delete data.roommatePreferences;
+        arrayRoommate.forEach((t) => {
+          data = {
+            roommatePreferences: { ...data.roommatePreferences, ...t },
+          };
+        });
+        arrayProperty.forEach((t) => {
+          data = {
+            ...data,
+            propertyPreferences: { ...data.propertyPreferences, ...t },
+          };
+        });
+        console.log(data);
+        formRoom.setFieldsValue(data);
+        formProp.setFieldsValue(data);
+      });
+    };
+    asyncGetUser();
+  }, [formRoom, nickname, formProp]);
+  useEffect(() => {
+    if (fieldsRoom) {
+      console.log("RoommatePreferences => ", fieldsRoom);
+      var preferencesRoomate = Object.entries(fieldsRoom.roommatePreferences);
+      preferencesRoomate = preferencesRoomate.filter((t) => t[1]);
+      const attributes = preferencesRoomate.map((a) => {
+        return { attributeType: a[0], value: a[1], weigth: 0 };
+      });
+      let bodyReq = { attributes };
+      console.log(bodyReq);
+      let asyncPutUser = async () => {
+        await ApiRequest.put(`/user/preferences/roommate/${idUser}`, bodyReq).then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            notification.success({
+              message: `Datos Actualizados`,
+              placement: "bottomLeft",
+            });
+            //history.push(`/profile/${bodyReq.userNickname}`);
+          } else {
+            notification.error({
+              message: `Error: No se pudo actualizar sus datos`,
+              placement: "bottomLeft",
+            });
+          }
+        });
+      };
+      asyncPutUser();
+    }
+  }, [fieldsRoom,idUser]);
+  useEffect(() => {
+    if (fieldsProp) {
+      console.log("PropertyPreferences => ", fieldsProp);
+    }
+  }, [fieldsProp]);
+  return (
+    <ContentWrapper header footer>
+      <CustomizedForm
+        form={formRoom}
+        data={userPreferenciesRoomie}
+        onfinish={setFieldsRoom}
+      />
+      <CustomizedForm
+        form={formProp}
+        data={userPreferenciesProperty}
+        onfinish={setFieldsProp}
+      />
+    </ContentWrapper>
+  );
+};
+
+export default UpdatePreferenciesForm;
