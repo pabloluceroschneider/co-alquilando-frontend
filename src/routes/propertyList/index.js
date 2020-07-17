d6667d28960602c81db307e890f0269668efdd53
BrunoConti <bruno.conti1997@gmail.com>
2020-07-17 18:17:30 -0300

    Add: primera version de pagina

--- src/routes/propertyList/index.js
+++ src/routes/propertyList/index.js
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import ApiRequest from "../../util/ApiRequest"
-import PropertyCard from '../../components/PropertyCard/index'
-import {notification} from 'antd'
-import '../../styles/PropertyList.css'
+import React, { useEffect, useState } from "react";
+import ApiRequest from "../../util/ApiRequest";
+import PropertyCard from "../../components/PropertyCard/index";
+import { notification } from "antd";
+import "../../styles/PropertyList.css";
 
 const Property = () => {
-    const [datos, setDatos] = useState(null)
-    useEffect(
-        () => {
-            let asyncGet = async () => {
-                try {
-                    let{data} = await ApiRequest.get("/property/properties");
-                    setDatos(data)
-                    console.log(data)
-                } catch (e) {
-                    notification.error({
-                        message: `Error: ${e.message}`,
-                        placement: 'bottomLeft'
-                    });
-                }
-            }
-            asyncGet()
+  const [datos, setDatos] = useState(null);
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useSize(20);
+  useEffect(() => {
+    let asyncGet = async () => {
+      try {
+        let { content } = await ApiRequest.get(
+          `/property/properties?page=${page}&size${size}`
+        );
+        setDatos(content);
+        console.log(content);
+      } catch (e) {
+        notification.error({
+          message: `Error: ${e.message}`,
+          placement: "bottomLeft",
+        });
+      }
+    };
+    asyncGet();
+  }, []);
 
-        }, []
-    )
 
-    return (
-
-        <div className="contentPL" >
-            {datos? datos.map((p)=>{
-                return(
-                    <PropertyCard {...p} />
-                )
-            }  ):null}
-           
-        </div>
-        
-
-    )
-}
+  return (
+    <div className="contentPL">
+      {datos
+        ? datos.map((p) => {
+            return <PropertyCard {...p} />;
+          })
+        : null}
+    </div>
+  );
+};
 
 export default Property;
