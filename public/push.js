52b9e51b4a6b6456abd085d710b8bb625126fa04
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-08-18 20:51:49 -0300

    Coneccion con server push

---
+++ public/push.js
@@ -0,0 +1,105 @@
+navigator.serviceWorker.register("./sw.js").then(initialiseState)
+
+
+function initialiseState() {
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        console.warn('Notifications aren\'t supported.');
+        return;
+    }
+
+    if (Notification.permission === 'denied') {
+        console.warn('The user has blocked notifications.');
+        return;
+    }
+
+    if (!('PushManager' in window)) {
+        console.warn('Push messaging isn\'t supported.');
+        return;
+    }
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+    console.log("serviceWorkerRegistration: ", serviceWorkerRegistration)
+        serviceWorkerRegistration.pushManager.getSubscription().then(function (subscription) {
+            if (!subscription) {
+                subscribe();
+                return;
+            }
+            
+            // Keep your server in sync with the latest subscriptionId
+            // sendSubscriptionToServer(subscription);
+            console.log("subscription -->", subscription)
+        })
+        .catch(function(err) {
+            console.warn('Error during getSubscription()', err);
+        });
+    });
+}
+
+function subscribe() {
+    const publicKey = base64UrlToUint8Array('BAPGG2IY3Vn48d_H8QNuVLRErkBI0L7oDOOCAMUBqYMTMTzukaIAuB5OOcmkdeRICcyQocEwD-oxVc81YXXZPRY');
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        serviceWorkerRegistration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: publicKey
+        })
+        .then(function (subscription) {
+            return sendSubscriptionToServer(subscription);
+        })
+        .catch(function (e) {
+            if (Notification.permission === 'denied') {
+                console.warn('Permission for Notifications was denied');
+            } else {
+                console.error('Unable to subscribe to push.', e);
+            }
+        });
+    });
+}
+
+function sendSubscriptionToServer(subscription) {
+    var key = subscription.getKey ? subscription.getKey('p256dh') : '';
+    var auth = subscription.getKey ? subscription.getKey('auth') : '';
+
+    console.log({
+        endpoint: subscription.endpoint,
+        key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+        auth: auth ? btoa(String.fromCharCode.apply(null, new Uint8Array(auth))) : ''
+    });
+
+
+    // Normally, you would actually send the subscription to the server:
+    let formData = new FormData();
+    formData.append('subscriptionJson', subscription);
+    return fetch('http://localhost:8080/notifications/send', {
+        method: 'POST',
+        body: formData
+    });
+    // return fetch('http://localhost:8080/notifications/send', {
+    //     credentials: 'include',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     method: 'POST',
+    //     body: JSON.stringify({
+    //         endpoint: subscription.endpoint,
+    //         key: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : '',
+    //         auth: auth ? btoa(String.fromCharCode.apply(null, new Uint8Array(auth))) : ''
+    //     })
+    // });
+}
+
+function base64UrlToUint8Array(base64UrlData) {
+    const padding = '='.repeat((4 - base64UrlData.length % 4) % 4);
+    const base64 = (base64UrlData + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = atob(base64);
+    const buffer = new Uint8Array(rawData.length);
+    
+    for (let i = 0; i < rawData.length; ++i) {
+        buffer[i] = rawData.charCodeAt(i);
+    }
+    
+    return buffer;
+}
--- src/sw.js
+++ src/sw.js
@@ -1,17 +1,22 @@
 
 class ServiceWorker{
-
-    static register(){
-        console.log("register")
-        Notification.requestPermission(function(result) {
-            if (result === 'denied') {
-              console.log('Permission wasn\'t granted. Allow a retry.');
-              return;
-            } else if (result === 'default') {
-              console.log('The permission request was dismissed.');
-              return;
-            }
-          });
-    }
+  static register(){
+    console.log("register")
+    Notification.requestPermission(function(result) {
+      if (result === 'denied') {
+        console.log('Permission wasn\'t granted. Allow a retry.');
+        return;
+      } else if (result === 'default') {
+        console.log('The permission request was dismissed.');
+        return;
+      }
+      if ('serviceWorker' in navigator) {
+        console.log("he")
+        navigator.serviceWorker.register('/sw.js');
+      }else{
+        console.log("NO")
+      }
+    });
+  }
 }
 export default ServiceWorker;
