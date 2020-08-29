a4cf169b70e3ac8f3e3698ed780b7cfe4a7b9454
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-08-29 14:26:39 -0300

    Agrego archivo separado para traducciones "es"

--- src/i18n.js
+++ src/i18n.js
@@ -1,40 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import es from './util/translations/en.json';
 
 // the translations
-// (tip move them in a JSON file and import them)
 const resources = {
-  en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next",
-
-      "typology": "Tipología",
-      "APARMENT": "Departamento",
-      "HOUSE": "Casa",
-      "available": "Disponible",
-      "disabled": "No disponible",
-      "pre_rented": "Reservada",
-      "rented": "Rentada",
-
-      "amountPeople": "Cantidad de personas",
-      "rooms": "Habitaciones",
-      "bathrooms": "Baños",
-      "gym": "Gimnacio",
-      "pool": "Pileta",
-      "playroom": "Salon de juegos",
-      "roaster": "Asador",
-      "garage": "Garage",
-      "balcony": "Balcón",
-      "elevator": "Ascensor",
-      "furnished": "Amoblado",
-      "aa": "Aire acondicionado",
-      "sum": "Salon de usos múltiples",
-      "calefaction": "Calefacción",
-      "pets": "Acepta mascotas",
-      "security": "Seguridad",
-      "true": "Sí",
-      "false": "No"
-    }
+  es:{
+    translation : es
   }
 };
 
@@ -42,7 +13,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en",
+    lng: "es",
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
