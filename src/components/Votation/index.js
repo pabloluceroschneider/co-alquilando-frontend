959e58bcf34d6b08df4c04668af7fd4141cc7480
Santiago Costa <santiago.costa.m@gmail.com>
2020-09-29 19:26:26 -0300

    estilado de votacion sin historial

--- src/components/Votation/index.js
+++ src/components/Votation/index.js
@@ -1,61 +1,64 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from "react-router-dom";
 import { Rate } from 'antd';
-import { CheckSquareOutlined, CloseSquareOutlined, CheckCircleOutlined } from '@ant-design/icons';
+import { CloseCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
 import { SessionContext } from '../../store';
 import ApiRequest from '../../util/ApiRequest';
 import Property from '../../classes/Property';
-
+import '../../styles/VotationState.css';
+import { convertLegacyProps } from 'antd/lib/button/button';
 
 const useVotations = () => {
-    const [data, setData] = useState(null); 
-    const [votations, setVotations] = useState(null); 
+    const [data, setData] = useState(null);
+    const [votations, setVotations] = useState(null);
     let { group } = useParams();
 
-    useEffect( () => {
+
+
+    useEffect(() => {
         const getVotations = async () => {
             const { data } = await ApiRequest.get(`/group/votation/all/${group}`);
             setData(data);
-            };
+        };
         getVotations();
     }, [])
 
-    useEffect( () => {
-        if(!data) return;
+    useEffect(() => {
+        if (!data) return;
 
         let ongoing = {}
         let history = []
-        data.map( item => {
-            if (item.result === "ongoing"){
-                ongoing = item 
+        data.map(item => {
+            if (item.result === "ongoing") {
+                ongoing = item
                 let votos = Object.entries(item.votes)
-                ongoing = {...ongoing, votospositivos: votos.filter(voto => voto[1]).length }
-            }else{
+                ongoing = { ...ongoing, votospositivos: votos.filter(voto => voto[1]).length }
+            } else {
                 history = [...history, item]
             }
         })
-        setVotations({ongoing, history})
+        setVotations({ ongoing, history })
 
-    },[data])
+    }, [data])
 
     return [votations];
 }
 
-const OnGoing = ({item, detail}) => {
-    const [ property, setProperty ] = useState(null);
+const OnGoing = ({ item, detail }) => {
+    const [property, setProperty] = useState(null);
     const { state } = useContext(SessionContext);
-    
+
 
     useEffect(() => {
         if (!item) return;
         let asyncGetUser = async () => {
             let { data } = await ApiRequest.get(`/property/${item?.propertyId}`);
             let formatedProperty = new Property(data).mapResponseToJson();
-            console.log("Propiedad :", formatedProperty )
+            console.log("Propiedad :", formatedProperty)
             setProperty(formatedProperty);
         };
         asyncGetUser();
-    },[item]);
+    }, [item]);
 
     const handleVote = async vote => {
         let bodyReq = {
@@ -66,40 +69,63 @@ const OnGoing = ({item, detail}) => {
         let { data } = await ApiRequest.put(`/group/votation/vote/${detail?.id}`, bodyReq);
     }
 
+    let typologies = {
+        "ongoing": "En curso",
+        "passed": "Aprobada",
+        "failed": "Rechazada",
+        "canceled": "Cancelada"
+    }
+    console.log("prop", property)
     return (
         <div className="ongoing">
 
             <div className="votation">
-                <div className="title">{property?.title}</div>
-                <div className="buttons">
-                    <CheckSquareOutlined onClick={() => handleVote(true)}/>
-                    <CloseSquareOutlined onClick={() => handleVote(false)} />
+                <div className="votationHeader">
+                    <img
+                        alt="imagen de perfil"
+                        src={property?.photos[0]}
+                        className="imageVotation"
+                    />
+                    <div className="contentVotationTitle">
+                        <a href={`/property/${item?.propertyId}`} className="titleVotation">{property?.title}</a>
+                        <div className="priceVotation">Precio: ${property?.price.rentPrice}</div>
+                        <div className="buttonsVotation">
+                            <CheckCircleOutlined className="buttonsVotationOk" onClick={() => handleVote(true)} />
+                            <CloseCircleOutlined className="buttonsVotationNo" onClick={() => handleVote(false)} />
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="rowVotation">
+                    <div className="subtitleVotation">Resultado actual</div>
+                    <Rate className="actualVotation" character={<CheckCircleOutlined />} disabled count={detail?.membersId.length} value={item?.votospositivos} />
+                </div>
+
+                <div className="rowVotation">
+                    <div className="resultVotation">{typologies[item?.result]}</div>
                 </div>
             </div>
-
-            <div className="results"> 
-                <Rate character={<CheckCircleOutlined />} disabled count={detail?.membersId.length} value={item?.votospositivos} />
-            </div>
-
-            <div className="state">
-                state
-            </div>
+ 
         </div>
     )
 }
-const History = ({items}) => {
+const History = ({ items }) => {
     return (
-        <div className="history">History</div>
+       <div className="history">
+           Historial
+        </div>
+
     )
 }
 
-const Votation = ({detail}) => {
-    const [ votations ] = useVotations();
+const Votation = ({ detail }) => {
+    const [votations] = useVotations();
     console.log("detalle del grupo :", detail)
 
     return (
         <div className="votation-wrapper">
-            <OnGoing item={votations?.ongoing} detail={detail}/>
+            <OnGoing item={votations?.ongoing} detail={detail} />
             <History items={votations?.history} />
         </div>
     )
