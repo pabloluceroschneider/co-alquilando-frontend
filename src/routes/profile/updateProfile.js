eab577c1746d1942675a2f6ed3de4a42e5d20311
agustinperalta <43344932+agustinperalta@users.noreply.github.com>
2020-10-05 20:26:20 -0300

    Bugfix

--- src/routes/profile/updateProfile.js
+++ src/routes/profile/updateProfile.js
@@ -17,13 +17,6 @@ const userData = {
     primaries: [
       [
         {
-          label: "Nombre de usuario",
-          name: "userNickname",
-          component: "Input",
-        },
-      ],
-      [
-        {
           label: "Nombre",
           name: "userName",
           component: "Input",
