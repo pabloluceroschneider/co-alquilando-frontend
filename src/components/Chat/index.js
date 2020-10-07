9bf43de34687c50f097445b54163852d75e16973
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-10-06 22:02:32 -0300

    Cambio state -> status

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,39 +1,19 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { SessionContext } from '../../store';
 import WebSocket from '../WebSocket';
-import ApiRequest from '../../util/ApiRequest';
-import { notification } from 'antd';
 
 const Chat = ({ render, groupId, channel, channelName }) => {
-	const [ datos, setDatos ] = useState(null);
   	const { state } = useContext(SessionContext);
-  
-	useEffect(() => {
-			let asyncGet = async () => {
-				try {
-					let { data } = await ApiRequest.get(`/group/user/${state.user.id}`);
-					setDatos(data);
-				} catch (e) {
-					notification.error({
-						message: `Error: ${e.message}`,
-						placement: 'bottomLeft'
-					});
-				}
-			};
-			asyncGet();
-		},[ state ]);
 
 	return (
 		<div key={channel} className={`chat-container ${!!render}`}>
-
-      <WebSocket 
-        name={state.user.userNickname} 
-        id={state.user.id} 
-        groupId={groupId} 
-		channel={channel} 
-		channelName={channelName}
-        />
-
+			<WebSocket 
+				name={state.user.userNickname} 
+				id={state.user.id} 
+				groupId={groupId} 
+				channel={channel} 
+				channelName={channelName}
+			/>
 		</div>
 	);
 };
