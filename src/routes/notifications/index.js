483306d8f0ef4f313ab88bdf9bcfae0d6da325ba
BrunoConti <bruno.conti1997@gmail.com>
2020-10-12 20:21:03 -0300

    Agregar control si no tienes notificaciones

--- src/routes/notifications/index.js
+++ src/routes/notifications/index.js
@@ -3,6 +3,8 @@ import { SessionContext } from "../../store";
 import ApiRequest from "../../util/ApiRequest";
 import ContentWrapper from "../../components/ContentWrapper";
 import NotificationCard from "../../components/NotificationCard";
+import WaitingSelection from '../../components/WaitingSelection';
+import { BellOutlined } from '@ant-design/icons';
 
 const Notifications = (props) => {
   const { state } = useContext(SessionContext);
@@ -18,6 +20,15 @@ const Notifications = (props) => {
     };
     asyncGet();
   }, [state.user.id]);
+
+  if (!notifications?.length) return (
+    <ContentWrapper topNav breadscrumb={breadscrumb} >
+        <div className="no-groups">
+            <WaitingSelection  message="No tienes notificaciones!" render={!notifications} icon={<BellOutlined />} />
+        </div>
+    </ContentWrapper>
+)
+
   return (
     <ContentWrapper topNav title="Notificaciones" breadscrumb={breadscrumb}>
       <div className="notifications">
