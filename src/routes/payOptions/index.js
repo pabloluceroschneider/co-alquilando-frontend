f22c9c23f6a219878187db871591d337a1ede425
Santiago Costa <santiago.costa.m@gmail.com>
2020-10-14 19:33:44 -0300

    Tarjetas de opciones de pago

---
+++ src/routes/payOptions/index.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import PayCard from '../../components/PayCard/index'
+import ContentWrapper from "../../components/ContentWrapper";
+import "../../styles/PayOptions.css"
+
+const PayOptions = () => {
+
+
+    return (
+
+        <ContentWrapper topNav>
+        <div className="contentPayOptions">
+        <PayCard title="Basico" price="299" propertiesAmount="5" desc="Ideal para propietarios individuales" color="#99A3A4" buyLink="https://www.mercadopago.com.ar/"></PayCard>
+        <PayCard title="Premium" price="690" color="#148F77" propertiesAmount="10" desc="Ideal para pequeños negocios" buyLink="https://www.mercadopago.com.ar/"></PayCard>
+        <PayCard title="Business" price="1640" color="#D4AC0D" propertiesAmount="20" buyLink="https://www.mercadopago.com.ar/" desc="Ideal para grandes negocios"></PayCard>
+        </div>
+        </ContentWrapper>
+    );
+
+
+}
+
+export default PayOptions;
