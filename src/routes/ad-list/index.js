64acc31ec1e2d6812aa35fe0e2ec6f8f42510e7e
BrunoConti <bruno.conti1997@gmail.com>
2021-03-08 21:48:09 -0300

    Agrego metodo get de publicidades

--- src/routes/ad-list/index.js
+++ src/routes/ad-list/index.js
@@ -1,39 +1,59 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from "react";
 import ContentWrapper from "../../components/ContentWrapper";
-import AdCard from '../../components/AdCard';
-import AdInfo from '../../components/AdInfo';
-import WaitingSelection from '../../components/WaitingSelection';
-import { HomeOutlined } from '@ant-design/icons';
-import Spin from '../../components/Spin';
+import AdCard from "../../components/AdCard";
+import AdInfo from "../../components/AdInfo";
+import { notification } from "antd";
+import ApiRequest from "../../util/ApiRequest";
+import WaitingSelection from "../../components/WaitingSelection";
+import { HomeOutlined } from "@ant-design/icons";
+import Spin from "../../components/Spin";
 
 const AdList = () => {
-    const [ datos] = useState(null);
-    const breadscrumb = [{'Publicidades': '/ads'}]
+  const [datos, setDatos] = useState(null);
+  const breadscrumb = [{ Publicidades: "/ads" }];
 
+  useEffect(() => {
+    let asyncGet = async () => {
+      try {
+        let { data } = await ApiRequest.get(`/ad`);
+        setDatos(data);
+      } catch (e) {
+        notification.error({
+          message: `Error al obtener propiedades`,
+          placement: "bottomLeft",
+        });
+      }
+    };
+    asyncGet();
+  }, []);
 
-    return (
-        <ContentWrapper topNav breadscrumb={breadscrumb}>
-          <div className="ads-list--content">
-              
-            <AdInfo count="0"/>
-        
-            <div className="ads">
-              {/* {!datos ? <Spin /> : null} */}
-    
-              {/* {datos?.length
-              ? datos.map((p) => { */}
-                  <AdCard />
-    
-              {/* {datos && !datos.length ? (
-                <div className="no-groups">
-                  <WaitingSelection  message="No tienes ninguna publicidad publicada" render={true} icon={<HomeOutlined />} />
-                </div>
-              ) :null} */}
+  return (
+    <ContentWrapper topNav breadscrumb={breadscrumb}>
+      <div className="ads-list--content">
+        <AdInfo count={datos?.length} />
+
+        <div className="ads">
+          {!datos ? <Spin /> : null}
+
+          {datos?.length
+            ? datos.map((a) => {
+                return <AdCard key={a.id} {...a} />;
+              })
+            : null}
+
+          {datos && !datos.length ? (
+            <div className="no-groups">
+              <WaitingSelection
+                message="No tienes ninguna publicidad publicada"
+                render={true}
+                icon={<HomeOutlined />}
+              />
             </div>
-    
-          </div>
-        </ContentWrapper>
-      );
-}
+          ) : null}
+        </div>
+      </div>
+    </ContentWrapper>
+  );
+};
 
-export default AdList
+export default AdList;
