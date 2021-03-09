a1d10e209386c5b12af94f9b5fc91fc12cc184e6
camila stock <camila.stock78@gmail.com>
2021-03-08 22:12:09 -0300

    Double pie chart

--- src/components/Charts/PieChart/index.js
+++ src/components/Charts/PieChart/index.js
@@ -1,68 +1,31 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { SessionContext } from '../../../store';
-import ApiRequest from '../../../util/ApiRequest';
 
-
-const props = {
-	data: [
-		['Task', 'Hours per Day'],
-		['Work', 11],
-		['Eat', 2],
-		['Commute', 2],
-		['Watch TV', 2],
-		['Sleep', 7]
-	]
-};
-
-const PieChart = () => {
-	const [metric, setMetric] = useState();
-	const { state, dispatch } = useContext(SessionContext);
+const PieChart = ({ metric, keys }) => {
 
 	window.google.charts.load('current', { packages: ['corechart'] });
 	window.google.charts.setOnLoadCallback(drawChart);
 
 
-	useEffect(
-		() => {
-			if (metric) return;
-			const getMetric = async () => {
-				console.log("UserId: ", state.user.id)
-				await ApiRequest.get(`metric/owner/${state.user.id}`)
-					.then(({ data }) => {
-						console.log("data: ", data)
-						setMetric(data);
-					})
-			};
-			getMetric();
-		},
-		[metric, state]
-	);
-
-
-
 	function drawChart() {
-		if (typeof (metric) != 'undefined' && metric != null) {
-			console.log(metric.propertiesAverage);
+		if (typeof (metric) != 'undefined' && metric != null && metric.id != null) {
 
 			let formated = {
 				data: metric.propertiesAverage
 			};
 
 			var data = window.google.visualization.arrayToDataTable(formated.data);
-
 			var options = {
 				title: formated.data[0][1]
 			};
+			var chart = new window.google.visualization.PieChart(document.getElementById(`piechart-${keys}`));
+
+			chart.draw(data, options);
 		}
-
-		var chart = new window.google.visualization.PieChart(document.getElementById('piechart'));
-
-		chart.draw(data, options);
 	}
 
 	return (
-		<div style={{ textAlign: '-webkit-center' }}>
-			<div id="piechart" style={{ width: 900, height: 500 }} />
+		<div style={{ textAlign: '-webkit-center' }} key={keys}>
+			<div id={`piechart-${keys}`} style={{ width: 800, height: 400 }} />
 		</div>
 	);
 };
--- src/routes/properties/index.js
+++ src/routes/properties/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ContentWrapper from '../../components/ContentWrapper';
 import PieChart from '../../components/Charts/PieChart';
+import { SessionContext } from '../../store';
+import ApiRequest from '../../util/ApiRequest';
 
 const Property = () => {
+
+	const [metric, setMetric] = useState();
+	const { state, dispatch } = useContext(SessionContext);
+
+	useEffect(
+		() => {
+			if (metric) return;
+			const getMetric = async () => {
+				console.log("UserId: ", state.user.id)
+				await ApiRequest.get(`metric/owner/${state.user.id}`)
+					.then(({ data }) => {
+						console.log("data: ", data)
+						setMetric(data);
+					})
+			};
+			getMetric();
+		},
+		[metric, state]
+	);
+
+
 	return (
 		<ContentWrapper topNav>
-			<PieChart />
+			<div>
+				<PieChart metric={metric} keys={1} />
+				<PieChart metric={metric} keys={2} />
+			</div>
 		</ContentWrapper>
 	);
 };
