4b8623fa4d0ed2b226f52ebe06d99359103a1768
BrunoConti <bruno.conti1997@gmail.com>
2021-03-08 21:46:59 -0300

    Actualizo form Post y Update de publicidad

--- src/forms/POST_AD.js
+++ src/forms/POST_AD.js
@@ -1,49 +1,56 @@
 const adFields = {
-	name: 'ad',
-	layout: 'vertical',
-	btnSubmit: 'Registrar Publicidad',
-	fields: {
-		primaries: [
-			[
-				{
-					label: 'Cliente',
-					name: 'client',
-					component: 'Input',
-					required: true
-				}
-			],
-			[
-				{
-					label: "Fecha de Inicio",
-					name: "startDate",
-					component: "DatePicker",
-					dependencies:['startDate'],
-					required: true,
-				},
-				{
-					label: "Fecha de Fin",
-					name: "endDate",
-					component: "DatePicker",
-					dependencies:['finishDate'],
-					required: true,
-				},
-				{
-					label: 'Precio',
-					name: ['adPrice', 'price'],
-					component: 'Input',
-					required: true
-				}
-			],
-            [
-				{
-					label: 'Cargar Imagen',
-					name: 'photoURL',
-					component: 'Upload',
-					aspect: 16/9
-				}
-			],
-		],
-	}
+  name: "ad",
+  layout: "vertical",
+  btnSubmit: "Registrar Publicidad",
+  fields: {
+    primaries: [
+      [
+        {
+          label: "Cliente",
+          name: "client",
+          component: "Input",
+          required: true,
+        },
+        {
+          label: "Precio",
+          name: "price",
+          component: "InputCurrency",
+          required: true,
+        },
+      ],
+      [
+        {
+          label: "Fecha de Pago",
+          name: "paymentDate",
+          component: "DatePicker",
+          dependencies: ["startDate"],
+          required: true,
+        },
+        {
+          label: "Fecha de Inicio",
+          name: "startDate",
+          component: "DatePicker",
+          dependencies: ["startDate"],
+          required: true,
+        },
+        {
+          label: "Fecha de Fin",
+          name: "endDate",
+          component: "DatePicker",
+          dependencies: ["finishDate"],
+          required: true,
+        },
+      ],
+      [
+        {
+          label: "Cargar Imagen",
+          name: "photoURL",
+          component: "Upload",
+          aspect: 16 / 9,
+        },
+      ],
+    ],
+  },
 };
 
 export default adFields;
--- src/forms/UPDATE_AD.js
+++ src/forms/UPDATE_AD.js
@@ -1,53 +1,60 @@
 const adFields = {
-	name: 'ad',
-	layout: 'vertical',
-	btnSubmit: 'Actualizar Publicidad',
-    btnDelete: 'Eliminar perfil',
-	titleDelete: 'Eliminar perfil',
-	deleteContentModal:
-		"¿Desea eliminar esta publicidad? Si selecciona 'Aceptar', no podrá recuperarla.",
-	fields: {
-		primaries: [
-			[
-				{
-					label: 'Cliente',
-					name: 'client',
-					component: 'Input',
-					required: true
-				}
-			],
-			[
-				{
-					label: "Fecha de Inicio",
-					name: "startDate",
-					component: "DatePicker",
-					dependencies:['startDate'],
-					required: true,
-				},
-				{
-					label: "Fecha de Fin",
-					name: "endDate",
-					component: "DatePicker",
-					dependencies:['finishDate'],
-					required: true,
-				},
-				{
-					label: 'Precio',
-					name: ['adPrice','price'],
-					component: 'Input',
-					required: true
-				}
-			],
-            [
-				{
-					label: 'Cargar Imagen',
-					name: 'photoURL',
-					component: 'Upload',
-					aspect: 16/9
-				}
-			],
-		],
-	}
+  name: "ad",
+  layout: "vertical",
+  btnSubmit: "Actualizar Publicidad",
+  btnDelete: "Eliminar Publicidad",
+  titleDelete: "Eliminar Publicidad",
+  deleteContentModal:
+    "¿Desea eliminar esta publicidad? Si selecciona 'Aceptar', no podrá recuperarla.",
+  fields: {
+    primaries: [
+      [
+        {
+          label: "Cliente",
+          name: "client",
+          component: "Input",
+          required: true,
+        },
+        {
+          label: "Precio",
+          name: "price",
+          component: "InputCurrency",
+          required: true,
+        },
+      ],
+      [
+        {
+          label: "Fecha de Pago",
+          name: "paymentDate",
+          component: "DatePicker",
+          dependencies: ["startDate"],
+          required: true,
+        },
+        {
+          label: "Fecha de Inicio",
+          name: "startDate",
+          component: "DatePicker",
+          dependencies: ["startDate"],
+          required: true,
+        },
+        {
+          label: "Fecha de Fin",
+          name: "endDate",
+          component: "DatePicker",
+          dependencies: ["finishDate"],
+          required: true,
+        },
+      ],
+      [
+        {
+          label: "Cargar Imagen",
+          name: "photoURL",
+          component: "Upload",
+          aspect: 16 / 9,
+        },
+      ],
+    ],
+  },
 };
 
 export default adFields;
