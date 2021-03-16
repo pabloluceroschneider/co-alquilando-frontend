b66580cd5c1c779cdcb59cae8d349788b643d717
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2021-03-15 21:32:16 -0300

    Agrego paso a paso

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,6 +4,9 @@ import desktopVideo from '../../assets/images/Home/Coalq.mp4'
 import mobileVideo from '../../assets/images/Home/mobile-coalq.mov'
 import Login from  '../../components/Login'
 import "../../styles/Home.scss"
+import { Steps, Divider } from 'antd';
+
+const { Step } = Steps;
 
 
 const Home = () => {
@@ -31,6 +34,39 @@ const Home = () => {
           </div>
 
         </section>
+
+        <section className='steps'>
+          <div className='step-by-step'>
+
+            <div className='step'>
+              <h1 className='title'>¿Buscás compañero de Alquiler?</h1>
+              <Steps current={3} progressDot direction="vertical">
+                <Step title="Registrate" description="This is a description. This is a description." />
+                <Step title="Personalizá tu Búsqueda" description="This is a description. This is a description." />
+                <Step title="Encontrá tu compañero ideal" description="This is a description. This is a description." />
+              </Steps>
+            </div>
+
+            <div className='step'>
+              <h1 className='title'>Alquilá tu propiedad</h1>
+              <Steps current={3} progressDot direction="vertical">
+                <Step title="Registrate" description="This is a description. This is a description." />
+                <Step title="Publicá tu propiedad" description="This is a description. This is a description." />
+                <Step title="Seguí publicando" description="This is a description. This is a description." />
+              </Steps>
+            </div>
+
+
+          </div>
+        </section>
+
+        <section className='contact-us'>
+
+        </section>
+
+        <footer className='footer'>
+          <span>footer</span>
+        </footer>
 
       </div>
   );
