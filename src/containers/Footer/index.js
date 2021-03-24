f94ad88ac40ad9ed9e36025176b0611c8ec8de5d
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2021-03-24 18:02:10 -0300

    Agrego contenido al footer

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import { TwitterOutlined } from '@ant-design/icons';
+
 import './styles.scss';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      Coalquilando. Todos los Derechos Reservados
+      <div className='footer__about'>
+        <Link >
+          CoAlquilando
+        </Link>
+      </div>
+      <div className='footer__contact-us'>
+        <Link to={`/contact-us`}>
+          Contactanos
+        </Link>
+      </div>
+      <div className='footer__media'>
+        <span>Redes Sociales</span>
+        <Link to=''> <TwitterOutlined /> Twitter</Link>
+      </div>
     </footer>
   );
 }
