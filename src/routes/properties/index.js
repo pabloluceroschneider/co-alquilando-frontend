a9321fa09899c1d128f7fd7f839e05539ab86bf4
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2021-04-03 19:56:28 -0300

    Fix err

--- src/routes/properties/index.js
+++ src/routes/properties/index.js
@@ -60,7 +60,7 @@ const Property = () => {
 								onClick={() => toggleActions(selected.id)}
 							>
 								<img
-									src={`http://localhost:8080//property/${selected.id}/photos/${selected.photos?[0]}`}
+									src={`http://localhost:8080/property/${selected.id}/photos/${selected.photos ? selected.photos[0] : null}`}
 									alt={selected.description}
 								/>
 								<summary>
