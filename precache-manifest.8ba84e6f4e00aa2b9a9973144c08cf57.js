4f7c7bf0d1e6cb7b9eb1ca7c9e62a4d825c3a588
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-08-21 11:41:06 -0300

    Updates

---
+++ precache-manifest.8ba84e6f4e00aa2b9a9973144c08cf57.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d67fda660f1dd25ca2becb09193bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e7f82389780f703fd64",
+    "url": "/static/css/2.38bff71c.chunk.css"
+  },
+  {
+    "revision": "bd46ad63dd054ab5bab0",
+    "url": "/static/css/main.eef16d71.chunk.css"
+  },
+  {
+    "revision": "5e7f82389780f703fd64",
+    "url": "/static/js/2.ffde8b3a.chunk.js"
+  },
+  {
+    "revision": "9ea08682af23fe8d06172f7d334388ee",
+    "url": "/static/js/2.ffde8b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd46ad63dd054ab5bab0",
+    "url": "/static/js/main.e4dcb6ce.chunk.js"
+  },
+  {
+    "revision": "a18b08d9d3c3cede6289",
+    "url": "/static/js/runtime-main.787c3fcb.js"
+  },
+  {
+    "revision": "a8a2fe17ef058f266444f6bbb45d5b65",
+    "url": "/static/media/Coalq.a8a2fe17.mp4"
+  },
+  {
+    "revision": "21d9889022a0be4b10bfd5173b1ed409",
+    "url": "/static/media/house-pin.21d98890.svg"
+  },
+  {
+    "revision": "94cc580fdafd5f46fe8420d7c3dcb948",
+    "url": "/static/media/logonav.94cc580f.png"
+  },
+  {
+    "revision": "4ef7ced12c6bb1f49872c2baaf0bab38",
+    "url": "/static/media/mobile-coalq.4ef7ced1.mov"
+  },
+  {
+    "revision": "4b180fdfeb820efb3a993ad8b9e9d332",
+    "url": "/static/media/publish_property.4b180fdf.jpg"
+  },
+  {
+    "revision": "80f8b2440f6f44314e59cc89f800d8a5",
+    "url": "/static/media/search_dept.80f8b244.jpg"
+  },
+  {
+    "revision": "da3f8628339caab379d6cf0afbe357d0",
+    "url": "/static/media/search_roommate.da3f8628.jpg"
+  },
+  {
+    "revision": "9c77a0cef596d87d42688274fba4e497",
+    "url": "/static/media/search_roommate_md.9c77a0ce.jpg"
+  },
+  {
+    "revision": "12d77a8c517a431396e63503d5023d7c",
+    "url": "/static/media/untitled-font-1.12d77a8c.woff"
+  },
+  {
+    "revision": "2e18da4a9a19dbda8cbec7469c3aea25",
+    "url": "/static/media/untitled-font-1.2e18da4a.svg"
+  },
+  {
+    "revision": "e15f330ee12a588b0b573811d2c1d9f0",
+    "url": "/static/media/untitled-font-1.e15f330e.ttf"
+  },
+  {
+    "revision": "ea1400e16ac196aefdee210c5ddc9588",
+    "url": "/static/media/untitled-font-1.ea1400e1.eot"
+  }
+]);
