fa9050b1f27245fbb8de5d9b69d90d87d4051542
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-08-21 13:04:55 -0300

    s

--- src/util/Api.js
+++ src/util/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const url = {
     local: "http://localhost:8080",
     pwa: "http://192.168.0.5:8080",
-    aws: "http://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080",
+    aws: "https://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080",
 }
 
 export default axios.create({
