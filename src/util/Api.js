4fcd9d9d56592069f5457b51fe664808d6c06a7b
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-08-21 12:35:40 -0300

    modify scope to aws url

--- src/util/Api.js
+++ src/util/Api.js
@@ -2,11 +2,12 @@ import axios from 'axios';
 
 const url = {
     local: "http://localhost:8080",
-    pwa: "http://192.168.0.5:8080"
+    pwa: "http://192.168.0.5:8080",
+    aws: "http://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080",
 }
 
 export default axios.create({
-    baseURL: url.local,
+    baseURL: url.aws,
     headers:{
         'Content-Type':'application/json'
     }
