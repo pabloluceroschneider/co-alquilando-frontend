553805ac1013432bdc9dba092c615f9d8eba4199
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-12-31 16:12:14 -0300

    Fix endpoint hostname

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
-
-const url = {
-    local: "http://localhost:8080",
-    pwa: "http://192.168.0.5:8080",
-    aws: "https://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080",
-}
+import hostname from './getHostName';
 
 export default axios.create({
-    baseURL: url.local,
+    baseURL: hostname,
     headers:{
         'Content-Type':'application/json'
     }
--- src/util/getHostName.js
+++ src/util/getHostName.js
@@ -1,12 +1,12 @@
 
 const getHostName = () => {
   const hostname = window.location.hostname;
-  const pwa = 'http://192.168.0.5:8080'
   const domainMap = {
     localhost: 'http://localhost:8080',
-    coalquilando: 'https://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080'
+    coalquilando: 'https://ec2-34-219-1-255.us-west-2.compute.amazonaws.com:8080',
+    pwa: "http://192.168.0.5:8080",
   }
-  return domainMap[hostname] || pwa;
+  return domainMap[hostname] || domainMap.coalquilando;
 }
 
 const hostname = getHostName()
